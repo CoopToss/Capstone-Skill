3b96d663bc9d6413a0f680371bc7528f4dc1cac3
Cooper Martin <156128377+CoopToss@users.noreply.github.com>
2024-06-12 12:42:43 -0400

    Add files via upload

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from config import Config
+from .database import db
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    from .routes import routes
+    app.register_blueprint(routes)
+
+    return app
